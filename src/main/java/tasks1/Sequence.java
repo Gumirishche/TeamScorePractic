b42f19de25423a6ea3223094dc19c0d4ecb6d4b0
Gumirishche <gumer2001@mail.ru>
2023-11-24 20:34:33 +0400

    Было убрано объявление переменных в одну строку, теперь вводить можно дробные числа, среднее значение может быть не целым, добавлено поле count, которое используется для подсчёта количества значений.

--- src/main/java/tasks1/Sequence.java
+++ src/main/java/tasks1/Sequence.java
@@ -6,20 +6,24 @@ public class Sequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean go = true;
-        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE, sum = 0, mean = 0;
+        double min = Integer.MAX_VALUE;
+        double max = Integer.MIN_VALUE;
+        double sum = 0;
+        double mean;
+        int count = 0;
         String str;
         while (go) {
             str = scanner.nextLine();
             if (str.isEmpty()) {
                 go = false;
             } else {
-                min = Math.min(Integer.parseInt(str), min);
-                max = Math.max(Integer.parseInt(str), max);
-                sum = sum + Integer.parseInt(str);
-                mean++;
+                min = Math.min(Double.parseDouble(str), min);
+                max = Math.max(Double.parseDouble(str), max);
+                sum = sum + Double.parseDouble(str);
+                count++;
             }
         }
-        mean = sum / mean;
+        mean = sum / count;
         System.out.println("Min = " + min + "\nMax = " + max + "\nSum = " + sum + "\nMean = " + mean);
     }
 }
