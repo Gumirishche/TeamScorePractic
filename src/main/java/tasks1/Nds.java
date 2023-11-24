bb74fdf575992a869e790288c28218e664e46e56
Gumirishche <gumer2001@mail.ru>
2023-11-24 22:34:47 +0400

    Сделано округление стоимости товара без НДС, также сохраняются уже округлёные числа. Также т.к. используется округление, присутсвует погрешность +/- копейка. Из-за использования типа double присутствует проблема с округлением.

--- src/main/java/tasks1/Nds.java
+++ src/main/java/tasks1/Nds.java
@@ -1,6 +1,5 @@
 package tasks1;
 
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Nds {
@@ -8,13 +7,12 @@ public class Nds {
         System.out.println("Введите стоимость торвара:");
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
-        double sum = Double.parseDouble(str);
-        sum = sum / 1.2;
-        double nds = sum * 0.2;
-        System.out.println("Стоимость товара без НДС: " + sum);
-        DecimalFormat format = new DecimalFormat("#.##");
-        //System.out.println("Сумма НДС в чеке: " + nds);
-        System.out.println("Сумма НДС в чеке: " + format.format(nds));
-        System.out.println("Сумма НДС для налоговой декларации: " + Math.round(nds));
+        long sum = Math.round(Double.parseDouble(str)*100);
+        sum = Math.round(sum / 1.2);
+        long nds = Math.round(sum * 0.2);
+        System.out.println("Стоимость товара без НДС: " + Double.parseDouble(String.valueOf(sum))/100);
+        double ndsInReceipt = Double.parseDouble(String.valueOf(nds)) / 100;
+        System.out.println("Сумма НДС в чеке: " + ndsInReceipt);
+        System.out.println("Сумма НДС для налоговой декларации: " + Math.round(ndsInReceipt));
     }
 }
