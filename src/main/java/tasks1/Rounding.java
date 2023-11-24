fd88e3f917584135e217e95ab5c6ec555c42f1e5
Gumirishche <gumer2001@mail.ru>
2023-11-24 20:30:32 +0400

    Добавлены метод round(float a), а также описание этого метода в Conclusion.md

--- src/main/java/tasks1/Rounding.java
+++ src/main/java/tasks1/Rounding.java
@@ -2,16 +2,19 @@ package tasks1;
 
 public class Rounding {
     private void rounding() {
-        System.out.println("              30.0, 10000.1, 12.5, 99.99, 0.0, -23.45, -4.5, -129.675");
-        System.out.println("Math.ceil():  " + Math.ceil(30.0) + ", " + Math.ceil(10000.1) + ", " + Math.ceil(12.5)
+        System.out.println("                      30.0, 10000.1, 12.5, 99.99, 0.0, -23.45, -4.5, -129.675");
+        System.out.println("Math.ceil():          " + Math.ceil(30.0) + ", " + Math.ceil(10000.1) + ", " + Math.ceil(12.5)
                 + ", " + Math.ceil(99.99) + ", " + Math.ceil(0.0) + ", " + Math.ceil(-23.45) + ",  " + Math.ceil(-4.5)
                 + ", " + Math.ceil(-129.675));
-        System.out.println("Math.floor(): " + Math.floor(30.0) + ", " + Math.floor(10000.1) + ", " + Math.floor(12.5)
+        System.out.println("Math.floor():         " + Math.floor(30.0) + ", " + Math.floor(10000.1) + ", " + Math.floor(12.5)
                 + ", " + Math.floor(99.99) + ",  " + Math.floor(0.0) + ", " + Math.floor(-23.45) + ",  " + Math.floor(-4.5)
                 + ", " + Math.floor(-129.675));
-        System.out.println("Math.round(): " + Math.round(30.0) + ",   " + Math.round(10000.1) + ",   " + Math.round(12.5)
+        System.out.println("Math.round(double a): " + Math.round(30.0) + ",   " + Math.round(10000.1) + ",   " + Math.round(12.5)
                 + ",   " + Math.round(99.99) + ",   " + Math.round(0.0) + ",   " + Math.round(-23.45) + ",    " + Math.round(-4.5)
                 + ",   " + Math.round(-129.675));
+        System.out.println("Math.round(float a):  " + Math.round(30.0f) + ",   " + Math.round(10000.1f) + ",   " + Math.round(12.5f)
+                + ",   " + Math.round(99.99f) + ",   " + Math.round(0.0f) + ",   " + Math.round(-23.45f) + ",    " + Math.round(-4.5f)
+                + ",   " + Math.round(-129.675f));
     }
 
     public static void main(String[] args) {
