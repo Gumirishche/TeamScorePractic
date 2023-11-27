45c3da289d60b03da482c69a5655ce67a894024b
Gumirishche <gumer2001@mail.ru>
2023-11-27 23:19:03 +0400

    Теперь НДС расчитивается разницей между общей суммой и суммой без НДС.

--- src/main/java/tasks1/Nds.java
+++ src/main/java/tasks1/Nds.java
@@ -7,10 +7,10 @@ public class Nds {
         System.out.println("Введите стоимость торвара:");
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
-        long sum = Math.round(Double.parseDouble(str)*100);
-        sum = Math.round(sum / 1.2);
-        long nds = Math.round(sum * 0.2);
-        System.out.println("Стоимость товара без НДС: " + Double.parseDouble(String.valueOf(sum))/100);
+        long totalSum = Math.round(Double.parseDouble(str) * 100);
+        long sum = Math.round(totalSum / 1.2);
+        long nds = totalSum - sum;
+        System.out.println("Стоимость товара без НДС: " + Double.parseDouble(String.valueOf(sum)) / 100);
         double ndsInReceipt = Double.parseDouble(String.valueOf(nds)) / 100;
         System.out.println("Сумма НДС в чеке: " + ndsInReceipt);
         System.out.println("Сумма НДС для налоговой декларации: " + Math.round(ndsInReceipt));
